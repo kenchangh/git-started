6a2a3b71af49434559ef03aca4235a04ed13882d
Maverick Chan <guanhao3797@gmail.com>
2014-06-25 18:14:43 +0800

    Added more parameters for more options

--- bash.sh
+++ bash.sh
@@ -1,19 +1,28 @@
 git-started() {
-  echo -e "\nEnter your new GitHub repository's name: "
+
+  vars="$@"
+  echo -e "\nEnter your new Git repository's name: "
   read repo_name
+
+  if [[ " ${vars[*]} " == *" github "* || $# -eq 0 ]]; then
+    echo 'Sending requests to GitHub to create repository...'
+    curl -u 'guanhao97:9f733ad9a8cca515a8c8ad3a67a6f1a9c6ec488b' https://api.github.com/user/repos -d '{"name":"'$repo_name'"}'
+  fi
   
-  echo 'Sending requests to GitHub to create repository...'
-  curl -u 'guanhao97:9f733ad9a8cca515a8c8ad3a67a6f1a9c6ec488b' https://api.github.com/user/repos -d '{"name":"'$repo_name'"}'
-  
-  if [[ ! -f ~/Desktop/$repo_name ]]; then
-    echo "Making repository now..."
-    mkdir ~/Desktop/$repo_name
+  if [[ " ${vars[*]} " == *" repo "* || $# -eq 0 ]]; then
+    if [[ ! -f ~/Desktop/$repo_name ]]; then
+      echo "Making repository now..."
+      mkdir ~/Desktop/$repo_name
+      cd ~/Desktop/$repo_name
+      
+      git init
+    fi
   fi
     
-  cd ~/Desktop/$repo_name
-  
-  git init
-  bash ~/Desktop/git-started/repogenerator.sh
+  if [[ " ${vars[*]} " == *" repo "* || $# -eq 0 ]]; then
+    bash ~/Desktop/git-started/repogenerator.sh
+  fi
+
   git add --all :/
   
   echo -e "\nEnter your commit message: "
